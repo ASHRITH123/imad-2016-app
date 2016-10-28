aceac834f2ba012a75b49312e1b7521d23885062
Shaik Saif Ali <saifshine7@gmail.com>
2016-10-29 02:44:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,6 +126,9 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/js/jseyes.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'js', 'jseyes.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
